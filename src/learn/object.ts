6f20970e537d395908b63e219cdf479eaf8c8484
HaNguyen051 <vanha03092005@gmail.com>
2025-10-02 23:37:35 +0700

    object with optional

--- src/learn/object.ts
+++ src/learn/object.ts
@@ -1,4 +1,54 @@
 
 export{} ; 
-console.log("video 92") ; 
+console.log("video 94") ; 
 
+
+
+
+
+
+const person :  {
+    name: string;
+    age: number;
+    address: string;
+    isHandsome: boolean;
+    }
+     = {
+    name  : "Ha" , 
+    age : 20 , 
+    address : "Ha Noi" ,  
+    isHandsome : true 
+    }
+
+console.log("Person 1 : " , person) ; 
+
+const printInfo = (people:  {
+    name: string;
+    age: number;
+    address: string;
+    isHandsome: boolean;
+    }) =>{
+        console.log("Your name is : " , people.name )
+}
+printInfo(person) ; 
+
+//optional 
+
+const student1: {
+    name: string;
+    address: string;
+    phone?: string;
+} = {
+    name : "Eric" , 
+    address : "Bac Ninh" , 
+    phone :  "918312312"  , 
+}
+
+const student2: {
+    name: string;
+    address: string;
+    phones?: string;
+} = {
+    name : "Eric" , 
+    address : "Bac Ninh" , 
+}
