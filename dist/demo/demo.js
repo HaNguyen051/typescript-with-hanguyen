64f742ef289728b692e5f9aac84791b4372f48f0
HaNguyen051 <vanha03092005@gmail.com>
2025-10-02 15:16:24 +0700

    type of data

--- dist/demo/demo.js
+++ dist/demo/demo.js
@@ -1,3 +1,8 @@
 "use strict";
 console.log("hello world");
 console.log("aadasdasdasd");
+//khai bao type cho typescript
+let username = 'haNguyen'; //string
+username.toLocaleUpperCase();
+const age = 25; //number
+const isHandsome = true; //boolean
--- src/demo/demo.ts
+++ src/demo/demo.ts
@@ -1,3 +1,15 @@
 
 console.log("hello world") ; 
 console.log("aadasdasdasd") ; 
+
+//khai bao type cho typescript
+
+let username:string = 'haNguyen' ; //string
+
+username.toLocaleUpperCase() ; 
+
+const age:number = 25 ;  //number
+
+const isHandsome:boolean = true ; //boolean
+
+
