80be8bb3881dab3bdbded892e5d2dafce2ace10f
HaNguyen051 <vanha03092005@gmail.com>
2025-10-02 15:28:08 +0700

    Arrays

--- src/demo/demo.ts
+++ src/demo/demo.ts
@@ -1,6 +1,5 @@
-
-console.log("hello world") ; 
-console.log("aadasdasdasd") ; 
+//chia file ts thanh nhung file rieng le 
+export {} ; 
 
 //khai bao type cho typescript
 
@@ -12,4 +11,11 @@ const age:number = 25 ;  //number
 
 const isHandsome:boolean = true ; //boolean
 
+//Arrays
+const score:number[] = [10,6,8] ; 
 
+const address:string[] =["hanoi" , 'bac ninh'] ; 
+ //option2 : generic
+const name2: Array<string> = ["hanoi" , 'bac ninh'] ;
+
+console.log("score : " , score) ; 
