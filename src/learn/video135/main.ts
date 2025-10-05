2d212eead9582c557c2568833b22ecaa32451dc8
HaNguyen051 <vanha03092005@gmail.com>
2025-10-05 12:59:54 +0700

    import * as name from url

--- src/learn/video135/main.ts
+++ src/learn/video135/main.ts
@@ -1,12 +1,16 @@
 
 
-import { minus as has, sum } from "./math";
+//import { minus as has, sum } from "./math";
+
+//all
+import * as allFile from "./math" ; 
+
 import { Animal } from "./animal";
 import Person from "./student";
 //import { Student } from "./student";
 
 
 
-console.log("minus : 10 -  1  = " , has(10 , 1));
+console.log("minus : 10 -  1  = " , allFile.minus(10 , 1));
 
 const hs = new Person(134 , " ha") ; 
