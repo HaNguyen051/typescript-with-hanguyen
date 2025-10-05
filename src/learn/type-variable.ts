c0a4cd9658360b20dc6f6a8ab8874ecaf909a036
HaNguyen051 <vanha03092005@gmail.com>
2025-10-05 07:18:17 +0700

    generic with type

--- src/learn/type-variable.ts
+++ src/learn/type-variable.ts
@@ -1,9 +1,22 @@
 export{} ; 
 //T : type 
 //U , V , X 
-
+//khia bao function  generic
 function merge<T, U> (a : T , b : U ){
     return [a , b] ;
 }
 
 const a = merge<string , number>("ha nguyen" , 20) ; 
+
+//khoi tao 
+
+type MyArrString = string[] ; 
+
+type MyArrNumber = number[] ; 
+
+type MyArr<T> = T[] ; 
+
+type MyData<T> = T ; 
+
+const b :MyArr<number> = [2 , 21, 321, 32, 2] ; 
+
